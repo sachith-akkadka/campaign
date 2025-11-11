3cf3d1646556af412c39a7196b146b712886fd95
Sachith Akkadka <sachithaakkadka@gmail.com>
2025-11-11 11:55:22 +0530

    Replace hardcoded API key with environment variable

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC07MDlsBwIoz3aekY2BkcRUPncO5bUzKM",
+  apiKey:  process.env.Firebase_API,
   authDomain: "campaign-tracker-c67e1.firebaseapp.com",
   projectId: "campaign-tracker-c67e1",
   storageBucket: "campaign-tracker-c67e1.appspot.com",
